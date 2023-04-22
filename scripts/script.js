3a81e2326c0829ec8867a0e589743e2c801a6bb5
luisa urrego <luisafernandaurrego89@gmail.com>
2023-04-22 00:39:59 -0500

    Avances con filtraciones

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 //Peticion para obtener la data del miniback
+let productos = [];
 const URL_API = "http://localhost:3000/"
 
 const getProductsApi = async (url) => {
     try {
         const { data }= await axios.get(url + "productos")
-        console.log(data)
+       // console.log(data)
         return data;
 
     }catch (error) {
@@ -24,19 +25,19 @@ const containerCardProducts = document.querySelector(".container__cards")
 console.log(containerCardProducts)
 
 //Pintar los productos dentro del contenedor
-const printProducts = async(container, products) => {
-    console.log(products)
+const printProducts = (container, products) => {
+    //console.log(products)
     //vaciar contenedor
     container.innerHTML = "";
     //Recorrer array
     products.forEach((product) => {
         container.innerHTML += `
-        <div class="card  container  " >
-        <div class = "row">
-            <img src=${product.image} "class="card-img-top col-4 " alt=${product.nombre}>
+        <div class="card " >
+       
+            <img src=${product.image} "class="card-img-top" alt=${product.nombre}>
             <div class="card-body">
                 <h5 class="card-title">${product.nombre}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">An item</li>
@@ -47,7 +48,7 @@ const printProducts = async(container, products) => {
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
             </div>
-        </div>
+       
 
         `
         
@@ -58,6 +59,28 @@ const printProducts = async(container, products) => {
 
 // Escuchar al evento cuando se recarga la pagina y cuando suceda esto se hace un callback para que se pinten los productos
 document.addEventListener ('DOMContentLoaded',  async () => {
-    const productos = await getProductsApi (URL_API)
+    productos = await getProductsApi (URL_API)
     printProducts(containerCardProducts, productos )
+    
+})
+
+//filtracion por categorias
+//creancion del array donde se encuentran todas las categorias existentes
+const categoriesProducts = [];
+productos = await getProductsApi (URL_API)
+productos.forEach ((item) =>{
+    //si no existe el producto dentro del array categoria
+    if (!categoriesProducts.includes  (item.categoria) ) {
+        // entonces se pushea
+        categoriesProducts.push(item.categoria)
+       
+
+    }
+})
+//console.log(categoriesProducts)
+
+categoriesProducts.forEach ( (item) =>{
+    const filtro = document.getElementsByClassName (item)[0];
+    console.log(filtro)
+
 })
